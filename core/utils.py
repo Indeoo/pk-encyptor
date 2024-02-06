8862e7338477db31ae8f20fb75367b28c962254e
Arsenii Venherak <arsenii.venherak@extreme-idea.com>
2024-02-06 16:34:54 +0200

    refactor

--- core/utils.py
+++ core/utils.py
@@ -1,22 +1,5 @@
-import base64
-
 from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-
-def generate_key(password):
-    salt = b'\x87\x12\xac\xa3\x91\xb9\xfe\xb4\x08\x0c\xac\x19\xc6d\x85'
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-        backend=default_backend()
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(password))
-    return key
+from sybil_engine.utils.decryptor import generate_key
 
 
 def encrypt_private_key(private_key, password):
